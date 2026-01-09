95fc06d60d67719183bdc5cb85af1fd475a83e62
Josip Rubčić <josiprubcic2004@gmail.com>
2026-01-09 16:10:05 +0100

    initial setup

---
+++ src/test/java/com/rassus/treciProjekt/TreciProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rassus.treciProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreciProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
